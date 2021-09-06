35d8f4683006d4381bd12044da89c8d2a1894258
azerroth11 <mallory.berthome@gmail.com>
2021-09-06 10:49:43 +0200

    *
    Fixed max width

--- map.js
+++ map.js
@@ -1,72 +1,11 @@
-let map = AmCharts.makeChart('mapdiv', {
-    type: 'map',
-    theme: 'dark',
-    projection: 'mercator',
-    panEventsEnabled: false,
-    backgroundColor: '#fff',
-    dragMap: false,
-    zoomOnDoubleClick: false,
-    backgroundAlpha: 0,
-    autoTransform: true,
-    balloon: {
-        adjustBorderColor: true,
-        color: '#000000',
-        cornerRadius: 2,
-        fillColor: '#FFFFFF',
-        fontSize: 24,
-        borderThickness: 0.5,
-        horizontalPadding: 4,
-        verticalPadding: 1,
-    },
-    zoomControl: {
-        zoomControlEnabled: false,
-        homeButtonEnabled: false,
-    },
-    dataProvider: {
-        map: 'europeHigh',
-        getAreasFromMap: true,
-        areas: [
-            {
-                id: 'DK',
-                showAsSelected: true,
-            },
-            {
-                id: 'US',
-                showAsSelected: true,
-            },
-            {
-                id: 'PL',
-                showAsSelected: true,
-            },
-            {
-                id: 'IS',
-                showAsSelected: true,
-            },
-        ],
-        lines: [
-            {
-                color: '#4095bf',
-                arc: 0.7,
-                latitudes: [56.2639, 51.9194],
-                longitudes: [9.5018, 19.1451],
-            },
-            {
-                color: '#4095bf',
-                arc: 0.7,
-                latitudes: [56.2639, 64.9631],
-                longitudes: [9.5018, -19.0208],
-            },
-        ],
-    },
-    areasSettings: {
-        zoom: false,
-        selectable: false,
-        color: '#B4B4B7',
-        colorSolid: '#4095bf',
-        selectedColor: '#4095bf',
-        outlineColor: '#666666',
-        outlineThickness: 0.2,
-        rollOverColor: '#DCE7F7',
-        rollOverOutlineColor: '#000000',
-    },
-})
+;<script>
+    let chart = am4core.create('chartdiv', am4maps.MapChart) chart.geodata =
+    am4geodata_worldHigh chart.projection = new am4maps.projections.Mercator()
+    // Create map polygon series let polygonSeries = chart.series.push( new
+    am4maps.MapPolygonSeries() ) polygonSeries.useGeodata = true let
+    polygonTemplate = polygonSeries.mapPolygons.template
+    polygonTemplate.tooltipText = '{name}' polygonTemplate.fill =
+    am4core.color('#74B266') let hs = polygonTemplate.states.create('hover')
+    hs.properties.fill = am4core.color('#367B25') polygonSeries.include = [
+    'PT', 'ES', 'FR', 'DE', 'BE', 'NL', 'IT', 'AT', 'GB', 'IE', 'CH', 'LU', ]
+</script>
